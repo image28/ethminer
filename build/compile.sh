0b488757ca55bd274a95a3981dba83b444f97dab
Kevin Macey <image28@slingshot.co.nz>
2021-04-18 09:52:59 +1200

    Added cuda compiler
    Still not working yet

--- build/compile.sh
+++ build/compile.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 CUR=`pwd`
-MINERFLAGS="-DBIN_KERN -DETH_ETHASHCL -DUSE_SYS_OPENCL -DBOOST_BIND_GLOBAL_PLACEHOLDERS" # -DETH_ETHASHCUDA
+MINERFLAGS="-DBIN_KERN -DETH_ETHASHCL -DUSE_SYS_OPENCL -DBOOST_BIND_GLOBAL_PLACEHOLDERS -DETH_ETHASHCUDA"
 CXX="/usr/bin/c++"
 CXXFLAGS="-isystem -Wall -Wno-unknown-pragmas -Wextra -Wno-error=parentheses -pedantic -Ofast -ffunction-sections -fdata-sections -std=c++11 -DNDEBUG"
 AR="/usr/bin/ar"
 RANLIB="/usr/bin/ranlib"
-COPY="/usr/bin/cmake"
+#COPY="/usr/bin/cmake"
+NVCC="/opt/cuda/bin/nvcc"
 CUDAINC="-I/opt/cuda/include"
 HWMON="-I$CUR/../libhwmon/.."
-LIBS="../libethcore/libethcore.a ../libpoolprotocols/libpoolprotocols.a ../libdevcore/libdevcore.a libethminer-buildinfo.a /usr/lib/libboost_system.a /usr/lib/libboost_thread.a  ../libethash-cl/libethash-cl.a ../libhwmon/libhwmon.a /usr/lib/libOpenCL.so /usr/lib/libboost_filesystem.a /usr/lib/libboost_thread.a /usr/lib/librt.so ../libdevcore/libdevcore.a /usr/lib/libboost_system.a /usr/lib/libethash.a -lpthread -ldl -ljsoncpp -lethash -lssl -lcrypto" # /opt/cuda/lib64/libcudart_static.a ../libethash-cuda/libethash-cuda.a 
+LIBS="../libethcore/libethcore.a ../libpoolprotocols/libpoolprotocols.a ../libdevcore/libdevcore.a libethminer-buildinfo.a /usr/lib/libboost_system.a /usr/lib/libboost_thread.a  ../libethash-cl/libethash-cl.a ../libethash-cuda/libethash-cuda.a ../libhwmon/libhwmon.a /usr/lib/libOpenCL.so /usr/lib/libboost_filesystem.a /usr/lib/libboost_thread.a /opt/cuda/lib64/libcudart_static.a /usr/lib/librt.so ../libdevcore/libdevcore.a /usr/lib/libboost_system.a /usr/lib/libethash.a -lpthread -ldl -ljsoncpp -lethash -lssl -lcrypto"
 
 # Generate buildinfo.h
 NAME="ethminer"
@@ -25,28 +26,28 @@ PROJECT_NAME_VER="$PROJECT_NAME.$PROJECT_VERSION"
 
 mkdir -p ethminer
 cd $CUR/ethminer
-#$CXX -o libethminer-buildinfo.o -I../ethminer -DPROJECT_NAME_VERSION="$PROJECT_NAME_VER" -DPROJECT_NAME="$NAME" -DPROJECT_VERSION=0.19.0 -DCOMMIT="$COMMIT" -DSYSTEM_NAME="$SYSTEM" -DSYSTEM_PROCESSOR="$ARCH" -DCOMPILER_ID="$COMPILER" -DCOMPILER_VERSION="$COMPILER_VERSION" -DBUILD_TYPE="$TYPE" -c $CUR/../ethminer/buildinfo.c 
-#$AR qc libethminer-buildinfo.a libethminer-buildinfo.o
+$CXX -o libethminer-buildinfo.o -I../ethminer -DPROJECT_NAME_VERSION="$PROJECT_NAME_VER" -DPROJECT_NAME="$NAME" -DPROJECT_VERSION=0.19.0 -DCOMMIT="$COMMIT" -DSYSTEM_NAME="$SYSTEM" -DSYSTEM_PROCESSOR="$ARCH" -DCOMPILER_ID="$COMPILER" -DCOMPILER_VERSION="$COMPILER_VERSION" -DBUILD_TYPE="$TYPE" -c $CUR/../ethminer/buildinfo.c 
+$AR qc libethminer-buildinfo.a libethminer-buildinfo.o
 
 # DEVICE MANAGER CORE
 mkdir -p $CUR/libdevcore 
 cd $CUR/libdevcore 
-#$CXX $MINERFLAGS $CXXFLAGS -o CommonData.cpp.o -c $CUR/../libdevcore/CommonData.cpp
-#$CXX $MINERFLAGS $CXXFLAGS -o FixedHash.cpp.o -c $CUR/../libdevcore/FixedHash.cpp
-#$CXX $MINERFLAGS $CXXFLAGS -o Log.cpp.o -c $CUR/../libdevcore/Log.cpp
-#$CXX $MINERFLAGS $CXXFLAGS -o Worker.cpp.o -c $CUR/../libdevcore/Worker.cpp
-#$AR qc libdevcore.a CommonData.cpp.o FixedHash.cpp.o Log.cpp.o Worker.cpp.o
-#$RANLIB libdevcore.a
+$CXX $MINERFLAGS $CXXFLAGS -o CommonData.cpp.o -c $CUR/../libdevcore/CommonData.cpp
+$CXX $MINERFLAGS $CXXFLAGS -o FixedHash.cpp.o -c $CUR/../libdevcore/FixedHash.cpp
+$CXX $MINERFLAGS $CXXFLAGS -o Log.cpp.o -c $CUR/../libdevcore/Log.cpp
+$CXX $MINERFLAGS $CXXFLAGS -o Worker.cpp.o -c $CUR/../libdevcore/Worker.cpp
+$AR qc libdevcore.a CommonData.cpp.o FixedHash.cpp.o Log.cpp.o Worker.cpp.o
+$RANLIB libdevcore.a
 
 # HARDWARE MONITOR
 mkdir -p $CUR/libhwmon 
 cd $CUR/libhwmon 
-#$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wraphelper.cpp.o -c $CUR/../libhwmon/wraphelper.cpp
-#$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wrapnvml.cpp.o -c $CUR/../libhwmon/wrapnvml.cpp
-#$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wrapadl.cpp.o -c $CUR/../libhwmon/wrapadl.cpp
-#$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wrapamdsysfs.cpp.o -c $CUR/../libhwmon/wrapamdsysfs.cpp
-#$AR qc libhwmon.a wraphelper.cpp.o wrapnvml.cpp.o wrapadl.cpp.o wrapamdsysfs.cpp.o
-#$RANLIB libhwmon.a
+$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wraphelper.cpp.o -c $CUR/../libhwmon/wraphelper.cpp
+$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wrapnvml.cpp.o -c $CUR/../libhwmon/wrapnvml.cpp
+$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wrapadl.cpp.o -c $CUR/../libhwmon/wrapadl.cpp
+$CXX $MINERFLAGS $HWMON $CUDAINC $CXXFLAGS -o wrapamdsysfs.cpp.o -c $CUR/../libhwmon/wrapamdsysfs.cpp
+$AR qc libhwmon.a wraphelper.cpp.o wrapnvml.cpp.o wrapadl.cpp.o wrapamdsysfs.cpp.o
+$RANLIB libhwmon.a
 
 # POOL MANAGER
 mkdir -p $CUR/libpoolprotocols 
@@ -54,22 +55,23 @@ mkdir -p $CUR/libpoolprotocols/testing
 mkdir -p $CUR/libpoolprotocols/stratum
 mkdir -p $CUR/libpoolprotocols/getwork
 cd $CUR/libpoolprotocols 
-#$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o PoolURI.cpp.o -c $CUR/../libpoolprotocols/PoolURI.cpp
-#$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o PoolManager.cpp.o -c $CUR/../libpoolprotocols/PoolManager.cpp 
-#$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o testing/SimulateClient.cpp.o -c $CUR/../libpoolprotocols/testing/SimulateClient.cpp
-#$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o stratum/EthStratumClient.cpp.o -c $CUR/../libpoolprotocols/stratum/EthStratumClient.cpp
-#$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o getwork/EthGetworkClient.cpp.o -c $CUR/../libpoolprotocols/getwork/EthGetworkClient.cpp
+$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o PoolURI.cpp.o -c $CUR/../libpoolprotocols/PoolURI.cpp
+$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o PoolManager.cpp.o -c $CUR/../libpoolprotocols/PoolManager.cpp 
+$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o testing/SimulateClient.cpp.o -c $CUR/../libpoolprotocols/testing/SimulateClient.cpp
+$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o stratum/EthStratumClient.cpp.o -c $CUR/../libpoolprotocols/stratum/EthStratumClient.cpp
+$CXX $MINERFLAGS -I$CUR/../libpoolprotocols/.. -I$CUR $CXXFLAGS -o getwork/EthGetworkClient.cpp.o -c $CUR/../libpoolprotocols/getwork/EthGetworkClient.cpp
 $AR qc libpoolprotocols.a PoolURI.cpp.o PoolManager.cpp.o testing/SimulateClient.cpp.o stratum/EthStratumClient.cpp.o getwork/EthGetworkClient.cpp.o
 $RANLIB libpoolprotocols.a
 
 # CUDA KERNELS
-# -Xptxas "
-# --allow-expensive-optimizations
-# --fmad ?
-# --maxrregcount 12288 #(48kb of 4 byte registers)
-# --warn-on-double-precision-use
 # -Xptxas "--use_fast_math,--allow-expensive-optimizations,--fmad,--maxrregcount 12288,--warn-on-double-precision-use"
+mkdir -p $CUR/libethash-cuda
+cd $CUR/libethash-cuda
+$NVCC -I/opt/cuda/include -I$CUR/../ -I$CUR/../libethash-cuda --ptxas-options=-v --use_fast_math --disable-warnings -gencode arch=compute_35,code=sm_35 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_53,code=sm_53 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_62,code=sm_62 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 --fmad=true --maxrregcount=256 -DNVCC -o ethash-cuda_generated_ethash_cuda_miner_kernel.cu.o -c $CUR/../libethash-cuda/ethash_cuda_miner_kernel.cu
+$CXX $MINERFLAGS $CUDAINC -I$CUR/../libethash-cuda -I$CUR/libethash-cuda -I$CUR/.. $CXXFLAGS -o CUDAMiner.cpp.o -c $CUR/../libethash-cuda/CUDAMiner.cpp
 
+$AR qc libethash-cuda.a CUDAMiner.cpp.o ethash-cuda_generated_ethash_cuda_miner_kernel.cu.o
+$RANLIB libethash-cuda.a
 
 # OPENCL KERNEL
 # Convert ethash.cl to a c header file
@@ -98,4 +100,3 @@ $CXX $MINERFLAGS -I$CUR/../ethminer -I$CUR/.. $CUDAINC -I$CUR $CXXFLAGS -o main.
 
 $RANLIB libethminer-buildinfo.a
 $CXX $CXXFLAGS -static-libstdc++ main.cpp.o -o ethminer $LIBS
-
